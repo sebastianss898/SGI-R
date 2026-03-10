a0a02e78c333e80e72e3b934da9edc90f38243c3
Usuario <user@example.com>
2026-03-10 13:30:15 -0500

    implememtar llogo y suario

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-  // src/firebase.js
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSODyeLd3Y7DXmY1djmvLm4QL7vcbuwlQ",
-  authDomain: "sgi-01-8d1e2.firebaseapp.com",
-  databaseURL: "https://sgi-01-8d1e2-default-rtdb.firebaseio.com",
-  projectId: "sgi-01-8d1e2",
-  storageBucket: "sgi-01-8d1e2.firebasestorage.app",
-  messagingSenderId: "1070712317784",
-  appId: "1:1070712317784:web:c58f4635e011507e37a72d"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
